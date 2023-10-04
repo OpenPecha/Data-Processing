6f7280e5725375b05b703fa93a8f0c9acb73df5a
Eric86y <eric.werner.mail@gmail.com>
2023-10-04 23:16:26 +0200

    added class threshold,, some clean-up

--- generate_binary.py
+++ generate_binary.py
@@ -22,8 +22,10 @@ from Utils import create_directory, sanity_check, remove_directory, generate_bin
 def generate_masks(
     directory: str,
     target_class: str = "TextLine",
-    filter_blank: str = "yes",
-    overlay_preview: str = "no",
+    filter_blank: bool = True,
+    filter_threshold: str = True,
+    threshold: float = 0.005,
+    overlay_preview: bool = True
 ) -> None:
     _images = natsorted(glob(f"{directory}/*.jpg"))
     _xml = natsorted(glob(f"{directory}/page/*.xml"))
@@ -44,21 +46,26 @@ def generate_masks(
 
     create_directory(output_dir)
 
+
     for _img, _xml in tqdm(zip(_images, _xml), total=len(_images)):
         image_n = os.path.basename(_img).split(".")[0]
         img = cv2.imread(_img)
         mask = generate_binary_mask(img, _xml, target_class)
 
-        if filter_blank == "yes":
+        if filter_blank:
             """
             skips all masks that are entirely black, i.e. containing no class information
             """
             if np.sum(mask) == 0:
                 continue
-            else:
+
+        if filter_threshold:
+            tmp_mask = mask / 255
+
+            if np.sum(tmp_mask) > (mask.shape[0] * mask.shape[1]) * threshold:
                 mask_out = f"{output_dir}/{image_n}_mask.png"
 
-                if overlay_preview == "yes":
+                if overlay_preview:
                     cv2.addWeighted(mask, 0.4, img, 1 - 0.4, 0, img)
                     cv2.imwrite(mask_out, img)
 
@@ -68,7 +75,7 @@ def generate_masks(
         else:
             mask_out = f"{output_dir}/{image_n}_mask.png"
 
-            if overlay_preview == "yes":
+            if overlay_preview:
                 cv2.addWeighted(mask, 0.4, img, 1 - 0.4, 0, img)
                 cv2.imwrite(mask_out, img)
 
@@ -79,18 +86,24 @@ def generate_masks(
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_dir", type=str, required=True)
-    parser.add_argument("--target_class", type=str, required=True, default="TextLine")
+    parser.add_argument("--target_class", type=str, required=False, default="TextLine")
     parser.add_argument(
         "--filter_blank", choices=["yes", "no"], required=False, default="yes"
     )
     parser.add_argument(
+        "--filter_threshold", choices=["yes", "no"], required=False, default="yes"
+    )
+    parser.add_argument(
         "--overlay", choices=["yes", "no"], required=False, default="no"
     )
+    parser.add_argument("--threshold", type=float, required=False, default=0.005)
 
     args = parser.parse_args()
     input_dir = args.input_dir
     target_class = args.target_class
-    filter_blank = args.filter_blank
-    overlay = args.overlay
+    filter_blank = True if args.filter_blank == "yes" else False
+    filter_threshold = True if args.filter_threshold == "yes" else False
+    overlay = True if args.overlay == "yes" else False
+    threshold = args.threshold
 
-    generate_masks(input_dir, target_class, filter_blank, overlay)
+    generate_masks(input_dir, target_class, filter_blank, filter_threshold, threshold, overlay)
--- generate_binary_patched.py
+++ generate_binary_patched.py
@@ -21,12 +21,14 @@ from natsort import natsorted
 from Utils import create_directory, sanity_check, generate_binary_mask, patch_image
 
 
+
 def generate_masks(
     directory: str,
     target_class: str = "TextLine",
-    patch_size: int = 256,
-    filter_blank: str = "yes",
-    overlay_preview: str = "no",
+    filter_blank: bool = True,
+    filter_threshold: str = True,
+    threshold: float = 0.005,
+    overlay_preview: bool = True
 ) -> None:
     _images = natsorted(glob(f"{directory}/*.jpg"))
     _xml = natsorted(glob(f"{directory}/page/*.xml"))
@@ -41,6 +43,8 @@ def generate_masks(
     output_dir = os.path.join(mask_dir, f"Patched_Binary_{patch_size}")
     output_img_patches = os.path.join(output_dir, "Images")
     output_mask_patches = os.path.join(output_dir, "Masks")
+
+    filter_threshold = True
 
     create_directory(mask_dir)
     create_directory(output_dir)
@@ -59,18 +63,22 @@ def generate_masks(
             _img = img_patch.reshape(patch_size, patch_size, 3)
             _mask = mask_patch.reshape(patch_size, patch_size, 3)
 
-            if filter_blank == "yes":
+            if filter_blank:
                 """
                 skips all masks that are entirely black, i.e. containing no class information
                 """
                 if np.sum(_mask) == 0:
                     continue
 
-                else:
+            if filter_threshold:
+                tmp_mask = _mask / 255
+
+                if np.sum(tmp_mask) > (mask.shape[0] * mask.shape[1]) * threshold:
+
                     img_out = f"{output_img_patches}/{image_n}_{idx}.jpg"
                     mask_out = f"{output_mask_patches}/{image_n}_{idx}_mask.png"
 
-                    if overlay_preview == "yes":
+                    if overlay_preview:
                         cv2.addWeighted(_mask, 0.4, _img, 1 - 0.4, 0, _img)
                         cv2.imwrite(mask_out, _img)
 
@@ -78,24 +86,42 @@ def generate_masks(
                         cv2.imwrite(img_out, _img)
                         cv2.imwrite(mask_out, _mask)
 
+            else:
+                img_out = f"{output_img_patches}/{image_n}_{idx}.jpg"
+                mask_out = f"{output_mask_patches}/{image_n}_{idx}_mask.png"
+
+                if overlay_preview:
+                    cv2.addWeighted(_mask, 0.4, _img, 1 - 0.4, 0, _img)
+                    cv2.imwrite(mask_out, _img)
+
+                else:
+                    cv2.imwrite(img_out, _img)
+                    cv2.imwrite(mask_out, _mask)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_dir", type=str, required=True)
-    parser.add_argument("--target_class", type=str, required=True, default="TextLine")
+    parser.add_argument("--target_class", type=str, required=False, default="TextLine")
     parser.add_argument("--patch_size", required=False, type=int, default=256)
     parser.add_argument(
         "--filter_blank", choices=["yes", "no"], required=False, default="yes"
     )
     parser.add_argument(
+        "--filter_threshold", choices=["yes", "no"], required=False, default="yes"
+    )
+    parser.add_argument(
         "--overlay", choices=["yes", "no"], required=False, default="no"
     )
+    parser.add_argument("--threshold", type=float, required=False, default=0.005)
 
     args = parser.parse_args()
     input_dir = args.input_dir
     target_class = args.target_class
     patch_size = args.patch_size
-    filter_blank = args.filter_blank
-    overlay = args.overlay
+    filter_blank = True if args.filter_blank == "yes" else False
+    filter_threshold = True if args.filter_threshold == "yes" else False
+    overlay = True if args.overlay == "yes" else False
+    threshold = args.threshold
 
-    generate_masks(input_dir, target_class, patch_size, filter_blank, overlay)
+    generate_masks(input_dir, target_class, filter_blank, filter_threshold, threshold, overlay)
